f7fc5ad2562c441f1003aed3cac377423829ab2f
eQuantum <derlisa567@gmail.com>
2026-04-08 14:23:30 -0300

    Update CategoriesMarquee.jsx

--- components/CategoriesMarquee.jsx
+++ components/CategoriesMarquee.jsx
@@ -1,18 +1,19 @@
 import { categories } from "@/assets/assets";
+import Link from "next/link";
 
 const CategoriesMarquee = () => {
 
     return (
         <div className="overflow-hidden w-full relative max-w-7xl mx-auto select-none group sm:my-20">
 
-            {/* fade izquierda */}
             <div className="absolute left-0 top-0 h-full w-20 z-10 pointer-events-none bg-gradient-to-r from-white to-transparent" />
 
             <div className="flex min-w-[200%] animate-[marqueeScroll_10s_linear_infinite] sm:animate-[marqueeScroll_40s_linear_infinite] group-hover:[animation-play-state:paused] gap-4">
 
-                {[...categories, ...categories, ...categories, ...categories].map((company, index) => (
-                    <button
+                {[...categories, ...categories, ...categories, ...categories].map((category, index) => (
+                    <Link
                         key={index}
+                        href={`/shop?category=${encodeURIComponent(category)}`}
                         className="
                         px-5 py-2 
                         bg-[var(--accent)] 
@@ -24,15 +25,15 @@ const CategoriesMarquee = () => {
                         hover:text-white 
                         active:scale-95 
                         transition-all duration-300
+                        inline-block
                         "
                     >
-                        {company}
-                    </button>
+                        {category}
+                    </Link>
                 ))}
 
             </div>
 
-            {/* fade derecha */}
             <div className="absolute right-0 top-0 h-full w-20 md:w-40 z-10 pointer-events-none bg-gradient-to-l from-white to-transparent" />
 
         </div>
