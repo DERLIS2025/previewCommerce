fe5c11e0eb0a8e34313174fb051af133f5334e52
eQuantum <derlisa567@gmail.com>
2026-04-08 11:47:30 -0300

    Update OurSpec.jsx

--- components/OurSpec.jsx
+++ components/OurSpec.jsx
@@ -6,8 +6,11 @@ const OurSpecs = () => {
 
     return (
         <div className='px-6 my-20 max-w-6xl mx-auto'>
-            <Title visibleButton={false} title='Our Specifications' description="We offer top-tier service and convenience to ensure your shopping experience is smooth, secure and completely hassle-free." />
-
+            <Title
+  visibleButton={false}
+  title='Nuestras ventajas'
+  description='Te ofrecemos soluciones prácticas para que tu experiencia de compra sea más simple, clara y confiable.'
+/>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 gap-y-10 mt-26'>
                 {
                     ourSpecsData.map((spec, index) => {
