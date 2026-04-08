f9e5cee2745e4ee37a1cf6e75ebc1160a17fa116
eQuantum <derlisa567@gmail.com>
2026-04-08 11:33:07 -0300

    Update BestSelling.jsx

--- components/BestSelling.jsx
+++ components/BestSelling.jsx
@@ -5,14 +5,22 @@ import { useSelector } from 'react-redux'
 
 const BestSelling = () => {
 
-    const displayQuantity = 8
+    const displayQuantity = 4
     const products = useSelector(state => state.product.list)
+
+    const filteredProducts = products.filter(
+        product => product.section === 'riego-automatico'
+    )
 
     return (
         <div className='px-6 my-30 max-w-6xl mx-auto'>
-            <Title title='Best Selling' description={`Showing ${products.length < displayQuantity ? products.length : displayQuantity} of ${products.length} products`} href='/shop' />
-            <div className='mt-12  grid grid-cols-2 sm:flex flex-wrap gap-6 xl:gap-12'>
-                {products.slice().sort((a, b) => b.rating.length - a.rating.length).slice(0, displayQuantity).map((product, index) => (
+            <Title
+                title='Riego Automático'
+                description='Sistemas y accesorios de riego'
+                href='/shop'
+            />
+            <div className='mt-12 grid grid-cols-2 sm:flex flex-wrap gap-6 xl:gap-12'>
+                {filteredProducts.slice(0, displayQuantity).map((product, index) => (
                     <ProductCard key={index} product={product} />
                 ))}
             </div>
