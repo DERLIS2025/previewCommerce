02b4d0704e35e938049ddd9f52b855c9f5d99c7f
eQuantum <derlisa567@gmail.com>
2026-04-08 11:31:26 -0300

    Update LatestProducts.jsx

--- components/LatestProducts.jsx
+++ components/LatestProducts.jsx
@@ -9,11 +9,19 @@ const LatestProducts = () => {
     const displayQuantity = 4
     const products = useSelector(state => state.product.list)
 
+    const filteredProducts = products.filter(
+        product => product.section === 'cesped-natural'
+    )
+
     return (
         <div className='px-6 my-30 max-w-6xl mx-auto'>
-            <Title title='Latest Products' description={`Showing ${products.length < displayQuantity ? products.length : displayQuantity} of ${products.length} products`} href='/shop' />
+            <Title
+                title='Venta e Instalación'
+                description='Césped Natural'
+                href='/shop'
+            />
             <div className='mt-12 grid grid-cols-2 sm:flex flex-wrap gap-6 justify-between'>
-                {products.slice().sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)).slice(0, displayQuantity).map((product, index) => (
+                {filteredProducts.slice(0, displayQuantity).map((product, index) => (
                     <ProductCard key={index} product={product} />
                 ))}
             </div>
