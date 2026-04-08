29131b20743465abcfaf80c554750732ef546a72
eQuantum <derlisa567@gmail.com>
2026-04-08 11:48:34 -0300

    Update OurSpec.jsx

--- components/OurSpec.jsx
+++ components/OurSpec.jsx
@@ -7,18 +7,26 @@ const OurSpecs = () => {
     return (
         <div className='px-6 my-20 max-w-6xl mx-auto'>
             <Title
-  visibleButton={false}
-  title='Nuestras ventajas'
-  description='Te ofrecemos soluciones prácticas para que tu experiencia de compra sea más simple, clara y confiable.'
-/>
+                visibleButton={false}
+                title='Nuestras ventajas'
+                description='Te ofrecemos soluciones prácticas para que tu experiencia de compra sea más simple, clara y confiable.'
+            />
+
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 gap-y-10 mt-26'>
                 {
                     ourSpecsData.map((spec, index) => {
                         return (
-                            <div className='relative h-44 px-8 flex flex-col items-center justify-center w-full text-center border rounded-lg group' style={{ backgroundColor: spec.accent + 10, borderColor: spec.accent + 30 }} key={index}>
+                            <div
+                                className='relative h-44 px-8 flex flex-col items-center justify-center w-full text-center border rounded-lg group'
+                                style={{ backgroundColor: spec.accent + 10, borderColor: spec.accent + 30 }}
+                                key={index}
+                            >
                                 <h3 className='text-slate-800 font-medium'>{spec.title}</h3>
                                 <p className='text-sm text-slate-600 mt-3'>{spec.description}</p>
-                                <div className='absolute -top-5 text-white size-10 flex items-center justify-center rounded-md group-hover:scale-105 transition' style={{ backgroundColor: spec.accent }}>
+                                <div
+                                    className='absolute -top-5 text-white size-10 flex items-center justify-center rounded-md group-hover:scale-105 transition'
+                                    style={{ backgroundColor: spec.accent }}
+                                >
                                     <spec.icon size={20} />
                                 </div>
                             </div>
